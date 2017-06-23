b94a4864f7b728f1cae1018e77796bbb15c7f4e0
Bowen Zhong <bz4e14@soton.ac.uk>
2017-06-23 23:13:43 +0800

    first working version downloader

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const zlib = require('zlib');
 const request = require('request');
+const download = require('download');
 const chalk = require('chalk');
 
-function requestList(startPage, endPage, pageSize, orderBy) {
+let dist = './downloaded_imgs/';
+
+function pageRequestList(startPage, endPage, pageSize, orderBy) {
   if (typeof startPage != 'number' && typeof endPage != 'number' && typeof pageSize != 'number') {
     throw new Error('startPage, endPage and pageSize should be number');
   }
@@ -22,32 +25,54 @@ function requestList(startPage, endPage, pageSize, orderBy) {
   return requestArray;
 }
 
-function performRequest(requestArray) {
+function performPageRequest(requestArray) {
   if (requestArray.length === 0) return;
-  requestArray.forEach(function(value, key, array) {
+  requestArray.forEach(function (value, key, array) {
     request.get(value, {
-        headers: {
-          'content-type':'application/json',
-          'accept-encoding': 'gzip, deflate, sdch, br',
-          'accept-language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4',
-          'accept-version': 'v1',
-          'authorization': 'Client-ID d69927c7ea5c770fa2ce9a2f1e3589bd896454f7068f689d8e41a25b54fa6042'
-        },
-        encoding: null
-      },function(error, response, body) {
-        if (error) {
-          return console.log(chalk.red('==Error==: '), error);
-        }
+      headers: {
+        'content-type': 'application/json',
+        'accept-encoding': 'gzip, deflate, sdch, br',
+        'accept-language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4',
+        'accept-version': 'v1',
+        'authorization': 'Client-ID d69927c7ea5c770fa2ce9a2f1e3589bd896454f7068f689d8e41a25b54fa6042'
+      },
+      encoding: null
+    }, function (error, response, body) {
+      if (error) {
+        return console.log(chalk.red('==Error==: '), error);
+      }
 
-        zlib.unzip(body, function(err, result) {
-          if (error) {
-            return console.log('unzip error', error);
-          }
-          console.log('result: ', result.toString('utf-8'));
-        });
+      return getPicUrlList(body);
+    });
+  });
+}
+
+function getPicUrlList(res) {
+  var picUrlList = [];
+  zlib.unzip(res, function (error, result) {
+    if (error) {
+      return console.log('unzip error', error);
+    }
+    result = JSON.parse(result.toString('utf-8'));
+    result.forEach(function(value, key, array) {
+      picUrlList.push({
+        id: value.id,
+        url: value.links.download
+      });
+    });
+    console.log(picUrlList);
+    downloadPics(picUrlList);
+  });
+}
+
+function downloadPics(urlList) {
+  urlList.forEach((value, key, array) => {
+    download(value.url)
+      .pipe(fs.createWriteStream(dist + value.id))
+      .on('close', () => {
+        console.log('Img ' + chalk.green(value.id) + ' has been downloaded');
       });
   });
 }
 
-performRequest(requestList(1,1,24,'latest'));
-
+performPageRequest(pageRequestList(1, 1, 24, 'latest'));
